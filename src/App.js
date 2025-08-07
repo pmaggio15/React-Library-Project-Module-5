f2ac23cc7254935862294f07550185673786007d
Your Name <you@example.com>
2025-08-06 20:26:34 -0600

    2nd commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,17 @@
-import logo from './logo.svg';
-import './App.css';
+import './index.css';
+import Nav from './componets/Nav'
+import Landing from './componets/Landing';
+import Highlights from './componets/Highlights';
+import Highlight from './componets/ui/Highlight';
+import Featured from './componets/Featured'
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Nav />
+      <Landing />
+      <Highlights />
+      <Featured />
     </div>
   );
 }

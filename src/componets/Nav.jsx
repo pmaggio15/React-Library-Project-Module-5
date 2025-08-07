af279d3e7e99d0cf92c706b105647e6de2b04913
Your Name <you@example.com>
2025-08-06 21:03:29 -0600

    navigation bar

--- src/componets/Nav.jsx
+++ src/componets/Nav.jsx
@@ -4,6 +4,14 @@ import LibraryLogo from '../assets/Library.svg'
 
 
 const Nav = () => {
+    function openMenu() {
+        document.body.classList += " menu--open";
+    }
+
+    function closeMenu() {
+        document.body.classList.remove("menu--open");
+    }
+
     return (
         <div>
             <div className="nav__container">
@@ -21,7 +29,7 @@ const Nav = () => {
                         Books
                         </a>
                     </li>
-                    <button className='btn__menu'>
+                    <button className='btn__menu' onClick={openMenu}>
                         <FontAwesomeIcon icon="bars" />
                     </button>
                     <li className='nav__icon'>
@@ -32,7 +40,7 @@ const Nav = () => {
                     </li>
                 </ul>
                 <div className="menu__backdrop">
-                    <button className='btn__menu btn__menu--close'>
+                    <button className='btn__menu btn__menu--close' onClick={closeMenu}>
                         <FontAwesomeIcon icon="times" />
                     </button>
                     <ul className='menu__links'>
